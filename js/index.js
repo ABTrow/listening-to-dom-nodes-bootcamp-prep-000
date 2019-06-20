426405c4c5162178e37a4e719c711cc82f473faa
Austin Trow <austintrow@gmail.com>
2019-06-20 13:33:36 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
 
-const main = document.getElementbyID('main');
+const main = document.getElementbyId('main');
 
 main.addEventListener('click', function(event) {
   alert('I was clicked!');
