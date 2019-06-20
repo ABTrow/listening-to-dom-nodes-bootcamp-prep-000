4cb8ab472db829e3787e61040f49d61c651dd2a7
Austin Trow <austintrow@gmail.com>
2019-06-20 13:44:46 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -17,12 +17,13 @@ input.addEventListener('keydown', function(e) {
   }
 })
 
-let divs = document.querySelectorAll('div');
-
-function bubble(e) {
-  console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+divs = document.querySelectorAll('div');
+ 
+function capture(e) {
+  console.log(this.firstChild.nodeValue.trim() + ' captured');
 }
-
+ 
 for (let i = 0; i < divs.length; i++) {
-  divs[i].addEventListener('click', bubble);
+  // set the third argument to `true`!
+  divs[i].addEventListener('click', capture, true);
 }
